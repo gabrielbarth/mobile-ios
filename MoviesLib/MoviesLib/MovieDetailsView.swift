8679f6fc6f7f23efddf335e89631ea89cc31105b
gabrielbarth <gabriel.silverio@stone.com.br>
2025-03-09 15:16:33 -0400

    chore: add project assets

---
+++ MoviesLib/MoviesLib/MovieDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsView.swift
+//  MoviesLib
+//
+//  Created by Gabriel Barth on 09/03/25.
+//
+
+import SwiftUI
+
+struct MovieDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieDetailsView()
+}
