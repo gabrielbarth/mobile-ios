00314a007e310793464ff49725e91800dd83937c
gabrielbarth <gabriel.silverio@stone.com.br>
2025-03-09 15:09:57 -0400

    starts MoviesLib project

---
+++ MoviesLib/MoviesLib/MoviesLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesLibApp.swift
+//  MoviesLib
+//
+//  Created by Gabriel Barth on 09/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MoviesLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
